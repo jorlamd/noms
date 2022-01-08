ac71a0953ff1673ba67becc919a51f6380005ad6
jorlamd <34373043+jorlamd@users.noreply.github.com>
2022-01-08 18:36:11 +0000

    Update main.py

--- noms/client/main.py
+++ noms/client/main.py
@@ -66,7 +66,7 @@ class Client:
 
     def search_query(self, name):
         params = dict(
-            q=name,
+            query=name,
             ds='Standard Reference',
             format='json'
         )
